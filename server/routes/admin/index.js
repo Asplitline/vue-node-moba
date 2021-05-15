30760e81c9355279b8a82d30098e46743111add4
myqz <ambition292@163.com>
2021-05-15 15:32:58 +0800

    Universal CRUD package

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,34 +1,45 @@
-const Category = require('../../models/Category')
 
+const inflection = require('inflection')
 module.exports = app => {
     const express = require('express')
-    const router = express.Router()
-
-    router.get('/categories', async (req, res) => {
-        const model = await Category.find().populate('parent').limit(10)
+    const router = express.Router({
+        // 合并路由参数，子级优先级高
+        mergeParams: true
+    })
+    router.get('/', async (req, res) => {
+        const queryOptions = {}
+        // Model.prototype.modelName
+        if (req.model.modelName === 'Category') {
+            queryOptions.populate = 'parent'
+        }
+        const model = await req.model.find().setOptions(queryOptions).limit(10)
         res.send(model)
     })
-    router.get('/categories/:id', async (req, res) => {
-        const model = await Category.findById(req.params.id)
+    router.get('/:id', async (req, res) => {
+        const model = await req.model.findById(req.params.id)
         res.send(model)
     })
-    router.post('/categories', async (req, res) => {
-        const model = await Category.create(req.body)
+    router.post('', async (req, res) => {
+        const model = await req.model.create(req.body)
         res.send(model)
     })
-    router.put('/categories/:id', async (req, res) => {
-        await Category.findByIdAndUpdate(req.params.id, req.body)
+    router.put('/:id', async (req, res) => {
+        await req.model.findByIdAndUpdate(req.params.id, req.body)
         res.send({
             message: '更新成功'
         })
     })
-    router.delete('/categories/:id', async (req, res) => {
-        await Category.findByIdAndDelete(req.params.id)
+    router.delete('/:id', async (req, res) => {
+        await req.model.findByIdAndDelete(req.params.id)
         res.send({
             message: '删除成功'
         })
     })
 
-    // notes path start '/admin/api' 
-    app.use('/admin/api', router)
+    // base url
+    app.use('/admin/api/reset/:resource', (req, res, next) => {
+        const modelName = inflection.classify(req.params.resource)
+        req.model = require(`../../models/${modelName}`)
+        next()
+    }, router)
 }
